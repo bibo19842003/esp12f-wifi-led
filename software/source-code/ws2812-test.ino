63a36dfd2c74897238a5d50e44f0b150cafbe8e0
bibo <bibo19842003@163.com>
2021-08-29 00:45:39 +0800

    add basic files

---
+++ software/source-code/ws2812-test.ino
@@ -0,0 +1,218 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WS2812FX.h>
+#include <WiFiManager.h>
+#include "pitches.h"
+
+extern const char index_html[];
+extern const char main_js[];
+
+
+// QUICKFIX...See https://github.com/esp8266/Arduino/issues/263
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+
+#define LED_PIN 14                       // 0 = GPIO0, 2=GPIO2
+#define LED_COUNT 8
+
+#define HTTP_PORT 80
+
+#define DEFAULT_COLOR 0xFF5900
+#define DEFAULT_BRIGHTNESS 128
+#define DEFAULT_SPEED 1000
+#define DEFAULT_MODE FX_MODE_STATIC
+
+// notes in the melody:
+int melody[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {4, 8, 8, 4, 4, 4, 4, 4};
+
+// speaker pin
+int speakerpin = D1;  // 用于测试
+
+// wifi button
+int wifibutton = 0;  //wifibutton
+int temp = 0;    // 用于读取按键状态的临时变量
+
+
+String modes = "";
+uint8_t myModes[] = {}; // *** optionally create a custom list of effect/mode numbers
+const int num_mode = 6;
+const int custom_mode[6] ={0,1,2,7,11,12};
+
+WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+ESP8266WebServer server(HTTP_PORT);
+
+void setup(){
+  Serial.begin(115200);
+  delay(500);
+  Serial.println("\n\nStarting...");
+
+  // set pinmode
+  pinMode(wifibutton, INPUT); // 按键设置为输出
+  pinMode(speakerpin, OUTPUT);   // LED设置为输入
+
+  modes.reserve(5000);
+  modes_setup();
+
+  Serial.println("WS2812FX setup");
+  ws2812fx.init();
+  ws2812fx.setMode(DEFAULT_MODE);
+  ws2812fx.setColor(DEFAULT_COLOR);
+  ws2812fx.setSpeed(DEFAULT_SPEED);
+  ws2812fx.setBrightness(DEFAULT_BRIGHTNESS);
+  ws2812fx.start();
+
+  Serial.println("Wifi setup");
+  // Connect to WiFi network
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("dragonfly");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+
+ 
+  Serial.println("HTTP server setup");
+  server.on("/", srv_handle_index_html);
+  server.on("/main.js", srv_handle_main_js);
+  server.on("/modes", srv_handle_modes);
+  server.on("/set", srv_handle_set);
+  server.onNotFound(srv_handle_not_found);
+  server.begin();
+  Serial.println("HTTP server started.");
+
+  Serial.println("ready!");
+}
+
+
+void loop() {
+  unsigned long now = millis();
+
+  server.handleClient();
+  ws2812fx.service();
+
+  // wifibutton
+  temp = digitalRead(wifibutton);
+  if (temp == HIGH) {
+    clearwifiinfor();
+  }
+
+//  delay(500);
+}
+
+
+
+/*
+ * Build <li> string for all modes.
+ */
+void modes_setup() {
+  modes = "";
+  for(uint8_t i=0; i < num_mode; i++) {
+    modes += "<li><a href='#'>";
+    modes += ws2812fx.getModeName(custom_mode[i]);
+    modes += "</a></li>";
+  }
+}
+
+/* #####################################################
+#  Webserver Functions
+##################################################### */
+
+void srv_handle_not_found() {
+  server.send(404, "text/plain", "File Not Found");
+}
+
+void srv_handle_index_html() {
+  server.send_P(200,"text/html", index_html);
+}
+
+void srv_handle_main_js() {
+  server.send_P(200,"application/javascript", main_js);
+}
+
+void srv_handle_modes() {
+  server.send(200,"text/plain", modes);
+}
+
+void srv_handle_set() {
+  for (uint8_t i=0; i < server.args(); i++){
+    if(server.argName(i) == "c") {
+      uint32_t tmp = (uint32_t) strtol(server.arg(i).c_str(), NULL, 10);
+      if(tmp >= 0x000000 && tmp <= 0xFFFFFF) {
+        ws2812fx.setColor(tmp);
+      }
+    }
+
+    if(server.argName(i) == "m") {
+      uint8_t tmp = (uint8_t) strtol(server.arg(i).c_str(), NULL, 10);
+      uint8_t new_mode = sizeof(myModes) > 0 ? myModes[tmp % sizeof(myModes)] : tmp % ws2812fx.getModeCount();
+      ws2812fx.setMode(custom_mode[new_mode]);
+      Serial.print("mode is "); Serial.println(ws2812fx.getModeName(ws2812fx.getMode()));
+    }
+
+    if(server.argName(i) == "k") {
+      if(server.arg(i)[0] == '1') {
+        mytone();
+      }
+      Serial.print("Sound Sound Sound"); 
+    }
+
+    if(server.argName(i) == "b") {
+      if(server.arg(i)[0] == '-') {
+        ws2812fx.setBrightness(ws2812fx.getBrightness() * 0.8);
+      } else if(server.arg(i)[0] == ' ') {
+        ws2812fx.setBrightness(min(max(ws2812fx.getBrightness(), 5) * 1.2, 255));
+      } else { // set brightness directly
+        uint8_t tmp = (uint8_t) strtol(server.arg(i).c_str(), NULL, 10);
+        ws2812fx.setBrightness(tmp);
+      }
+      Serial.print("brightness is "); Serial.println(ws2812fx.getBrightness());
+    }
+
+    if(server.argName(i) == "s") {
+      if(server.arg(i)[0] == '-') {
+        ws2812fx.setSpeed(max(ws2812fx.getSpeed(), 5) * 1.2);
+      } else if(server.arg(i)[0] == ' ') {
+        ws2812fx.setSpeed(ws2812fx.getSpeed() * 0.8);
+      } else {
+        uint16_t tmp = (uint16_t) strtol(server.arg(i).c_str(), NULL, 10);
+        ws2812fx.setSpeed(tmp);
+      }
+      Serial.print("speed is "); Serial.println(ws2812fx.getSpeed());
+    }
+    
+  }
+  server.send(200, "text/plain", "OK");
+}
+
+void mytone() {
+    Serial.println("LED Turned ON, buzzer on");
+
+    // iterate over the notes of the melody:
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+      // to calculate the note duration, take one second divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / noteDurations[thisNote];
+      tone(speakerpin, melody[thisNote], noteDuration);
+
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      // stop the tone playing:
+      noTone(8);  
+    }
+}
+
+void clearwifiinfor() {
+  Serial.println("it will clear password!!!");
+  WiFiManager wifiManager;
+  wifiManager.resetSettings();
+  Serial.println("it had cleared password!!!");
+}
